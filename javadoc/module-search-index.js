e4544e63fc2602cb0a732ec4f9bdddd9bbfbdc23
Areczek00 <137199442+Areczek00@users.noreply.github.com>
2024-02-10 12:27:30 +0100

    Add files via upload

---
+++ javadoc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"com.projekt_biblioteka"}];updateSearchResults();
---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.projekt_biblioteka","l":"com.projekt_biblioteka"}];updateSearchResults();
